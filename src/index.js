85d26929ad42c98aa209289e487d12925d683fe5
Kevin Brail <kevinabrail@gmail.com>
2019-03-13 15:17:30 -0400

    added persistent login state via local storage

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import Feature from './components/Feature';
 
 const store = createStore(
     reducers,
-    {},
+    {auth: { authenticated: localStorage.getItem('token') }},
     applyMiddleware(reduxThunk)
 );
 
